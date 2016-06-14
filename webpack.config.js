9d4b838e3fad5bc027b10a87e9a8d48cefc43d27
JuanMa Garrido <JuanMa.Garrido@gmail.com>
2016-06-14 21:26:20 +0200

    build

--- webpack.config.js
+++ webpack.config.js
@@ -63,7 +63,6 @@ module.exports = (function makeWebpackConfig () {
 
   config.plugins.push(
     new HtmlWebpackPlugin({
-      hash: true,
       template: './src/_public/index.html',
       inject: 'body'
     })
@@ -109,34 +108,3 @@ module.exports = (function makeWebpackConfig () {
 
   return config;
 }());
-
-
-/*
-
-var HtmlWebpackPlugin = require('../..');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
-var webpackMajorVersion = require('webpack/package.json').version.split('.')[0];
-
-module.exports = {
-  entry: './example.js',
-  output: {
-    path: __dirname + '/dist/webpack-' + webpackMajorVersion,
-    publicPath: '',
-    filename: 'bundle.js'
-  },
-  module: {
-    loaders: [
-      { test: /\.css$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader') },
-      { test: /\.png$/, loader: 'file-loader' }
-    ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'template.html'
-    }),
-    new ExtractTextPlugin('styles.css')
-  ]
-};
-
-
-*/
