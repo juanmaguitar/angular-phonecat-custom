8385c5bf3f3598078b55ad6e55a307435696339e
JuanMa Garrido <JuanMa.Garrido@gmail.com>
2016-06-14 18:24:34 +0200

    fixed view details

--- src/app/controllers/PhoneDetailCtrl.js
+++ src/app/controllers/PhoneDetailCtrl.js
@@ -1,28 +1,10 @@
 class PhoneDetailCtrl {
   constructor ($routeParams, Phone) {
-
-		Phone.get(
-			{ phoneId: $routeParams.phoneId },
-			function(phone) {
-				console.log("por aqui...");
-				this.mainImageUrl = phone.images[0];
-			}
-		).$promise.then(function(data) {
-				console.log("data received...");
-		    this.phone = data.toJSON();
-		    console.log(this.phone);
-		});
-
-  // 	User.get( {username: 'bob'} ).$promise.then(function(data) {
-		//     scope.user = data.toJSON();
-		// });
-
-		// this.phone = Phone.get(
-		// 	{ phoneId: $routeParams.phoneId },
-		// 	function(phone) {
-		// 		this.mainImageUrl = phone.images[0];
-		// 	}
-		// );
+		var phoneId = $routeParams.phoneId;
+		this.phone = Phone.get(
+			{ phoneId },
+			(phone) => this.mainImageUrl = phone.images[0]
+		);
 		this.setImage = function(imageUrl) {
 			this.mainImageUrl = imageUrl;
 		}
